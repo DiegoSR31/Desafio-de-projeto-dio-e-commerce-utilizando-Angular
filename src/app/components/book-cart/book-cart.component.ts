8e225663c030467d1192d71e15a20ae63dcd6f13
DiegoSR31 <diegosilvarodrigues31@gmail.com>
2022-07-21 19:55:27 -0300

    commit inicial

---
+++ src/app/components/book-cart/book-cart.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { catchError } from 'rxjs';
+
+@Component({
+  selector: 'app-book-cart',
+  templateUrl: './book-cart.component.html',
+  styleUrls: ['./book-cart.component.css']
+})
+
+export class BookCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
